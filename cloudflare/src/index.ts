cd44b8b62ec260ae5d1efd663a4ba01cf669cd8e
Samuel Macleod <smacleod@cloudflare.com>
2022-10-20 18:50:18 +0100

    update

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -25,6 +25,6 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response('Hello, World from andyjiang!');
+    return new Response('uniquen3');
   }
 };
